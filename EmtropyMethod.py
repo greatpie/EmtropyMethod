208605c575dce8008f0bb2c2c91315cbdd85bc64
greatpie <greatpie@live.com>
2020-02-01 21:04:24 +0800

    Clean fileds.

--- EmtropyMethod.py
+++ EmtropyMethod.py
@@ -1,16 +1,18 @@
 '''
 @Author: greatpie
 @Date: 2020-01-15 17:52:10
-@LastEditTime : 2020-02-01 19:37:33
+@LastEditTime : 2020-02-01 21:04:08
 @LastEditors  : Please set LastEditors
 @Description: For Xiaolu
 @FilePath: /CuteXiao/entropy.py
 '''
+
+
 class EmtropyMethod:
     def __init__(self, index, positive, negative, row_name):
         if len(index) != len(row_name):
             raise Exception('数据指标行数与行名称数不符')
-        if sorted(index.columns) != sorted(positive+negative):
+        if sorted(index.columns) != sorted(positive + negative):
             raise Exception('正项指标加负向指标不等于数据指标的条目数')
 
         self.index = index.copy().astype('float64')
@@ -20,14 +22,24 @@ class EmtropyMethod:
 
     def uniform(self):
         uniform_mat = self.index.copy()
-        min_index = {column:min(uniform_mat[column]) for column in uniform_mat.columns}
-        max_index = {column:max(uniform_mat[column]) for column in uniform_mat.columns}
+        min_index = {
+            column: min(uniform_mat[column])
+            for column in uniform_mat.columns
+        }
+        max_index = {
+            column: max(uniform_mat[column])
+            for column in uniform_mat.columns
+        }
         for i in range(len(uniform_mat)):
             for column in uniform_mat.columns:
                 if column in self.negative:
-                    uniform_mat[column][i] = (uniform_mat[column][i] - min_index[column]) / (max_index[column] - min_index[column])
+                    uniform_mat[column][i] = (
+                        uniform_mat[column][i] - min_index[column]) / (
+                            max_index[column] - min_index[column])
                 else:
-                    uniform_mat[column][i] = (max_index[column] - uniform_mat[column][i]) / (max_index[column] - min_index[column])
+                    uniform_mat[column][i] = (
+                        max_index[column] - uniform_mat[column][i]) / (
+                            max_index[column] - min_index[column])
 
         self.uniform_mat = uniform_mat
         return self.uniform_mat
@@ -39,7 +51,9 @@ class EmtropyMethod:
             raise Exception('你还没进行归一化处理，请先调用uniform方法')
         for column in p_mat.columns:
             sigma_x_1_n_j = sum(p_mat[column])
-            p_mat[column] = p_mat[column].apply(lambda x_i_j: x_i_j / sigma_x_1_n_j if x_i_j / sigma_x_1_n_j != 0 else 1e-6)
+            p_mat[column] = p_mat[column].apply(
+                lambda x_i_j: x_i_j / sigma_x_1_n_j
+                if x_i_j / sigma_x_1_n_j != 0 else 1e-6)
 
         self.p_mat = p_mat
         return p_mat
@@ -51,7 +65,10 @@ class EmtropyMethod:
             raise Exception('你还没计算比重，请先调用calc_probability方法')
 
         import numpy as np
-        e_j  = -(1 / np.log(len(self.p_mat)+1)) * np.array([sum([pij*np.log(pij) for pij in self.p_mat[column]]) for column in self.p_mat.columns])
+        e_j = -(1 / np.log(len(self.p_mat) + 1)) * np.array([
+            sum([pij * np.log(pij) for pij in self.p_mat[column]])
+            for column in self.p_mat.columns
+        ])
         ejs = pd.Series(e_j, index=self.p_mat.columns, name='指标的熵值')
 
         self.emtropy_series = ejs
@@ -66,42 +83,56 @@ class EmtropyMethod:
 
         return self.d_series
 
-    def calc_Weight(self):
+    def calc_weight(self):
         self.uniform()
         self.calc_probability()
         self.calc_emtropy()
         self.calc_emtropy_redundancy()
-        self.Weight = self.d_series / sum(self.d_series)
-        self.Weight.name = '权值'
-        return self.Weight
+        self.weight = self.d_series / sum(self.d_series)
+        self.weight.name = '权值'
+        return self.weight
 
     def calc_score(self):
-        self.calc_Weight()
+        self.calc_weight()
 
         import numpy as np
-        self.score = pd.Series(
-            [np.dot(np.array(self.index[row:row+1])[0], np.array(self.Weight)) for row in range(len(self.index))],
-            index=self.row_name, name='得分'
-        ).sort_values(ascending=False)
+        self.score = pd.Series([
+            np.dot(
+                np.array(self.index[row:row + 1])[0], np.array(self.weight))
+            for row in range(len(self.index))
+        ],
+                               index=self.row_name,
+                               name='得分').sort_values(ascending=False)
         return self.score
+
 
 if __name__ == '__main__':
 
     import pandas as pd
-    f = open('2015年全年全国各省(市区)主要经济指标排名.csv', encoding='utf8')
-    df = pd.read_csv(f)
+    df = pd.read_csv('灰色.csv')
     df = df.dropna().reset_index(drop=True)
-    #如果编辑解释器不是jupyter-notebook或命令行，用print函数包裹下面的代码
-    # print(df.head())
 
-    indexs = ["GDP总量增速", "人口总量", "人均GDP增速", "地方财政收入总额", "固定资产投资", "社会消费品零售总额增速", "进出口总额","城镇居民人均可支配收入", "农村居民人均可支配收入"]
-
+    indexs = ['污水排放强度X1', '废气排放强度X2', '化学需氧量X3', '氨氮X4', '二氧化硫X5', '氮氧化物X6']
     Positive = indexs
     Negative = []
 
-    province = df['地区']
-    index = df[indexs]
+    grouper = df.groupby('year')
 
-    em = EmtropyMethod(index, Negative, Positive, province)
-    # print(em.calc_Weight())
-    print(em.calc_score())
+    weightDf = pd.DataFrame()
+    scoreDf = pd.DataFrame()
+
+    for name, group in grouper:
+        groupeDf = group.reset_index()
+        province = groupeDf['region']
+        index = groupeDf[indexs]
+        em = EmtropyMethod(index, Negative, Positive, province)
+        em.calc_score()
+
+        weight = em.weight.rename(name)
+        score = em.score.rename(name)
+
+        weightDf = weightDf.append(weight)
+        scoreDf = scoreDf.append(score)
+
+    weightDf.to_csv('output/weight.csv')
+    scoreDf.T.to_csv('output/score.csv')
